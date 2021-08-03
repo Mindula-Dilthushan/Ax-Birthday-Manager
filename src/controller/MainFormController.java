d4f05a0a09f0a478f5623205ec9338256451fd63
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-08-03 10:09:27 +0530

    btn exit success

--- src/controller/MainFormController.java
+++ src/controller/MainFormController.java
@@ -9,29 +9,32 @@ import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 import util.Util;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Optional;
 
 public class MainFormController {
 
     public AnchorPane anchorLordPane;
-    public JFXButton btnNew;
-    public Label lblDate,lblTime;
-    public JFXButton btnView;
-    public JFXButton btnExit;
+    public JFXButton btnNew,btnExit,btnView;
+    public Label lblDate, lblTime;
 
     //Property Injection
     Util util = new Util();
 
-    public void initialize(){
+    public void initialize() {
         try {
             util.setAnchor(anchorLordPane, "NewForm.fxml");
-        }catch (Exception e){
+        } catch (Exception e) {
         }
         generateDate();
     }
@@ -39,7 +42,7 @@ public class MainFormController {
     public void btnNewOnAction(ActionEvent actionEvent) throws Exception {
         try {
             util.setAnchor(anchorLordPane, "NewForm.fxml");
-        }catch (Exception e){
+        } catch (Exception e) {
         }
     }
 
@@ -57,5 +60,14 @@ public class MainFormController {
     }
 
     public void btnExitOnAction(ActionEvent actionEvent) {
+        ButtonType ok = new ButtonType("OK", ButtonBar.ButtonData.OK_DONE);
+        ButtonType no = new ButtonType("NO", ButtonBar.ButtonData.CANCEL_CLOSE);
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Are You Sure ?", ok, no);
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.orElse(no) == ok) {
+            Stage exit_stage = (Stage) btnExit.getScene().getWindow();
+            exit_stage.close();
+        } else {
+        }
     }
 }
